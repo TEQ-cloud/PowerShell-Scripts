a2dd9ca5c4790a87f091982c792a2a53ec6fc33b
TEQ-cloud <quintenmjfl@gmail.com>
2024-06-17 16:59:37 +0200

    Add files via upload

---
+++ Create_Mailbox_For_All_Domain_Users.ps1
@@ -0,0 +1,9 @@
+﻿$Users = Get-ADUser -SearchBase "CN=Users,DC=quinten,DC=lan" -Filter { ObjectClass -eq 'user' -and Enabled -eq 'true'}
+foreach ($User in $Users) {
+    Get-User $User.SamAccountName | Enable-Mailbox -Database "JusticeDB"
+    if (Get-Mailbox $User.SamAccountName) {
+        Write-Host -ForegroundColor Green "Mailbox for"$User.SamAccountName"was created successfuly."
+        } else {
+        Wrire-Host -ForegroundColor Red "Mailbox for"$User.SamAccountName"could not be created."
+    } 
+}
